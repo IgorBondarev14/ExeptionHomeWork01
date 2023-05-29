8b87f1662a5157867adf86f005baa10abfa3564d
Игорь <garkol@bk.ru>
2023-05-29 09:55:27 +0300

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] sArray = {"first str", null, "third str"};
+        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12};
+//        division(10, 0);
+//        printValueByIndex(array,12);
+        printStringArray(sArray);
+
+        }
+        // division by zero
+        public static void division(double firstNum, double secondNum){
+            if(secondNum ==  0) throw new ArithmeticException("Деление на ноль невозможно!");
+            double result = firstNum/secondNum;
+            System.out.printf("Результат деления %f на %f равен %f.\n", firstNum, secondNum, result);
+        }
+        // out of bounds
+        public static void printValueByIndex(int[] array, int index){
+            if(index > array.length - 1) throw new ArrayIndexOutOfBoundsException("Элемента с данным индексом не существует");
+            else System.out.printf("Элемент с индексом %d равен %d.\n",index, array[index]);
+        }
+
+        // Null pointer exception
+        public static void printStringArray(String[] sArray){
+            for (String item: sArray){
+                if(item == null) throw new NullPointerException("Элемент отсутствует!!!");
+                System.out.println(item);
+            }
+        }
+
     }
-}
